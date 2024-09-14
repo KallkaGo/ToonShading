a0d48ed92325c5c3becb9495ef474b0f62614a5c
KallkaGo <765830637@qq.com>
2024-09-14 15:34:56 +0800

    fix: useKTX2Loader with draco

--- src/utils/useKTX2Loader.ts
+++ src/utils/useKTX2Loader.ts
@@ -1,11 +1,13 @@
 import { useGLTF } from "@react-three/drei"
 import { useThree } from "@react-three/fiber";
 import { KTX2Loader } from "three-stdlib";
+
+
 const ktx2Loader = new KTX2Loader();
 const useKTX2Loader = (url: string, isDraco: boolean, isMeshOpt: boolean) => {
   const gl = useThree((state) => state.gl);
   const useDraco = isDraco ? './public/libs/draco/' : false;
-  const gltf = useGLTF(url, undefined, undefined, (loader) => {
+  const gltf = useGLTF(url, useDraco, undefined, (loader) => {
     ktx2Loader.setTranscoderPath(`./public/libs/basis/`);
     //@ts-ignore
     loader.setKTX2Loader(ktx2Loader.detectSupport(gl));
