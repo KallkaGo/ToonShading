846f150019a88b3cf809195d28f96df422b62001
KallkaGo <765830637@qq.com>
2024-09-14 16:00:53 +0800

    refactor: useKTX2Loader

--- src/utils/useKTX2Loader.ts
+++ src/utils/useKTX2Loader.ts
@@ -4,10 +4,10 @@ import { KTX2Loader } from "three-stdlib";
 
 
 const ktx2Loader = new KTX2Loader();
-const useKTX2Loader = (url: string, isDraco: boolean, isMeshOpt: boolean) => {
+const useKTX2Loader = (url: string, isDraco: boolean = false, isMeshOpt: boolean = false) => {
   const gl = useThree((state) => state.gl);
   const useDraco = isDraco ? './libs/draco/' : false;
-  const gltf = useGLTF(url, useDraco, undefined, (loader) => {
+  const gltf = useGLTF(url, useDraco, isMeshOpt, (loader) => {
     ktx2Loader.setTranscoderPath(`./libs/basis/`);
     //@ts-ignore
     loader.setKTX2Loader(ktx2Loader.detectSupport(gl));
