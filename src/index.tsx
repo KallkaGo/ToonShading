f66d1720b68f4c752603bc78bdfd43642d690042
More <765830637@qq.com>
2024-11-06 00:14:43 +0800

    complete some modifications: - chore: Delete the garbled characters in the ignore file - feat: Add author information - style: Remove unused post-processing effects

--- src/index.tsx
+++ src/index.tsx
@@ -9,3 +9,8 @@ import VConsole from "vconsole";
 
 const root = document.getElementById("root") as HTMLDivElement;
 ReactDOM.createRoot(root).render(<App />);
+
+console.info(
+  "%cAuthor:KallkaGo",
+  "color: orange;background: ivory;font-size:26px;border: 2px solid black;padding:10px;text-shadow: 1px 1px grey;border-radius:11px;"
+);
