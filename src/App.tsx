3b4ac92aa0e31a407d81916b5de61882d07cb446
KallkaGo <765830637@qq.com>
2025-07-06 23:39:13 +0800

    build: remove share libs and component

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,10 @@
 import BrowserUtil from '@utils/Browser'
 import { useEffect } from 'react'
-import ShareMgr from './mgr/ShareMgr'
 import ThreeContainer from './three/ThreeContainer'
 import UIContainer from './ui/UIContainer'
 
 export default function App() {
   useEffect(() => {
-    ShareMgr.shared.init()
     BrowserUtil.init()
   }, [])
 
